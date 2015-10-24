53991ca5188aadebb7e8e04476f36b991a0451dd
Siddalingamurthy BG <siddushri2015@gmail.com>
2015-10-24 11:10:16 +0530

    With Adaptive Auto Layout

---
+++ Guru Kendra/User Entry/GKSignInViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GKSignInViewController.swift
+//  Guru Kendra
+//
+//  Created by Siddalingamurthy Gangadharappa on 10/24/15.
+//  Copyright © 2015 Adroit. All rights reserved.
+//
+
+import UIKit
+
+class GKSignInViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}

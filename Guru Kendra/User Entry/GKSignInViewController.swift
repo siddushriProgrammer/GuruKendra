827c1661e33057af3ed217d3e0429a2056f05dab
Siddalingamurthy BG <siddushri2015@gmail.com>
2015-11-06 11:41:29 +0530

    committing left outs

--- Guru Kendra/User Entry/GKSignInViewController.swift
+++ Guru Kendra/User Entry/GKSignInViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class GKSignInViewController: UIViewController {
+class GKSignInViewController: UIViewController,UITextFieldDelegate {
     
+    @IBOutlet var signInFields: [UITextField]!
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,33 @@ class GKSignInViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - Action Methods
+
+    @IBAction func signInAction(sender: UIButton) {
+        
+        guard let unwrappedUserName = signInFields[0].text?.characters.count > 0 ? signInFields[0].text : nil else {
+            return
+        }
+        
+        guard let unwrappedPassword = signInFields[1].text?.characters.count > 0 ? signInFields[1].text : nil else {
+            return
+        }
+        
+        print("Username: \(unwrappedUserName) and Password: \(unwrappedPassword) ")
+        
+    }
+
+    
+    // MARK: - Text Fileld Delegate
+    func textFieldDidBeginEditing(textField: UITextField) {
+        
+    }
+    
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool {
+        
+        return true;
     }
     
     
